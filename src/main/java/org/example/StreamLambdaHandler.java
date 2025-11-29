451a04a0ebea8e95175c9d2451318379916bf96d
romanovosad87 <roman.novosad@yahoo.com>
2025-11-29 21:28:24 +0100

    Invoke Priming Optimization technique

--- src/main/java/org/example/StreamLambdaHandler.java
+++ src/main/java/org/example/StreamLambdaHandler.java
@@ -2,22 +2,20 @@ package org.example;
 
 
 import com.amazonaws.serverless.exceptions.ContainerInitializationException;
-import com.amazonaws.serverless.proxy.internal.testutils.AwsProxyRequestBuilder;
 import com.amazonaws.serverless.proxy.internal.testutils.MockLambdaContext;
+import com.amazonaws.serverless.proxy.model.ApiGatewayRequestIdentity;
 import com.amazonaws.serverless.proxy.model.AwsProxyRequest;
+import com.amazonaws.serverless.proxy.model.AwsProxyRequestContext;
 import com.amazonaws.serverless.proxy.model.AwsProxyResponse;
 import com.amazonaws.serverless.proxy.spring.SpringBootLambdaContainerHandler;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
-import jakarta.ws.rs.HttpMethod;
-import jakarta.ws.rs.core.HttpHeaders;
-import jakarta.ws.rs.core.MediaType;
 import org.crac.Resource;
 import org.example.exception.CouldNotInitializeApplicationException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpMethod;
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -44,7 +42,7 @@ public class StreamLambdaHandler implements RequestStreamHandler, Resource {
     @Override
     public void beforeCheckpoint(org.crac.Context<? extends Resource> context) throws Exception {
         logger.info("Preparing snapshot (before checkpoint)");
-        warmUpHandler();
+        handler.proxy(getAwsProxyRequest(), new MockLambdaContext());
     }
 
     @Override
@@ -53,16 +51,17 @@ public class StreamLambdaHandler implements RequestStreamHandler, Resource {
     }
 
 
-    private void warmUpHandler() {
-        InputStream requestStream = new AwsProxyRequestBuilder("/reservation/1", HttpMethod.GET)
-                .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON)
-                .buildStream();
-        ByteArrayOutputStream responseStream = new ByteArrayOutputStream();
+    private static AwsProxyRequest getAwsProxyRequest () {
+        final AwsProxyRequest awsProxyRequest = new AwsProxyRequest();
+        awsProxyRequest.setHttpMethod(HttpMethod.GET.name());
+        awsProxyRequest.setPath("/reservation/1");
 
-        try {
-            handleRequest(requestStream, responseStream, new MockLambdaContext());
-        } catch (IOException e) {
-            logger.error("Warm-up request failed", e);
-        }
+        final AwsProxyRequestContext awsProxyRequestContext = new AwsProxyRequestContext();
+        final ApiGatewayRequestIdentity apiGatewayRequestIdentity= new ApiGatewayRequestIdentity();
+        apiGatewayRequestIdentity.setApiKey("key");
+        awsProxyRequestContext.setIdentity(apiGatewayRequestIdentity);
+        awsProxyRequest.setRequestContext(awsProxyRequestContext);
+
+        return awsProxyRequest;
     }
 }
