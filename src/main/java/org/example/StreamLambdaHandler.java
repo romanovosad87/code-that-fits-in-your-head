79180aa7fd3f036b6f6aa900547d56ec36d20467
romanovosad87 <roman.novosad@yahoo.com>
2025-11-29 20:55:50 +0100

    Invoke Priming Optimization technique

--- src/main/java/org/example/StreamLambdaHandler.java
+++ src/main/java/org/example/StreamLambdaHandler.java
@@ -2,18 +2,30 @@ package org.example;
 
 
 import com.amazonaws.serverless.exceptions.ContainerInitializationException;
+import com.amazonaws.serverless.proxy.internal.testutils.AwsProxyRequestBuilder;
+import com.amazonaws.serverless.proxy.internal.testutils.MockLambdaContext;
 import com.amazonaws.serverless.proxy.model.AwsProxyRequest;
 import com.amazonaws.serverless.proxy.model.AwsProxyResponse;
 import com.amazonaws.serverless.proxy.spring.SpringBootLambdaContainerHandler;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
+import jakarta.ws.rs.HttpMethod;
+import jakarta.ws.rs.core.HttpHeaders;
+import jakarta.ws.rs.core.MediaType;
+import org.crac.Resource;
 import org.example.exception.CouldNotInitializeApplicationException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
-public class StreamLambdaHandler implements RequestStreamHandler {
+public class StreamLambdaHandler implements RequestStreamHandler, Resource {
+
+    private static final Logger logger = LoggerFactory.getLogger(StreamLambdaHandler.class);
+
     private static final SpringBootLambdaContainerHandler<AwsProxyRequest, AwsProxyResponse> handler;
     static {
         try {
@@ -27,5 +39,30 @@ public class StreamLambdaHandler implements RequestStreamHandler {
     public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context)
             throws IOException {
         handler.proxyStream(inputStream, outputStream, context);
+    }
+
+    @Override
+    public void beforeCheckpoint(org.crac.Context<? extends Resource> context) throws Exception {
+        logger.info("Preparing snapshot (before checkpoint)");
+        warmUpHandler();
+    }
+
+    @Override
+    public void afterRestore(org.crac.Context<? extends Resource> context) throws Exception {
+        logger.info("Snapshot restored (after restore)");
+    }
+
+
+    private void warmUpHandler() {
+        InputStream requestStream = new AwsProxyRequestBuilder("/reservation/1", HttpMethod.GET)
+                .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON)
+                .buildStream();
+        ByteArrayOutputStream responseStream = new ByteArrayOutputStream();
+
+        try {
+            handleRequest(requestStream, responseStream, new MockLambdaContext());
+        } catch (IOException e) {
+            logger.error("Warm-up request failed", e);
+        }
     }
 }
